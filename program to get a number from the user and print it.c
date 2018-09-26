796438e1fdad5f053e7d010045b2925aa850cbb7
chetan0021 <42961971+chetan0021@users.noreply.github.com>
2018-09-26 17:52:28 +0530

    Update program to get a number from the user and print it.c

--- program to get a number from the user and print it.c
+++ program to get a number from the user and print it.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
+#include <stdio.h>//global declaration
 
-int main()
+int main()//local declaration
 {
-    int num;
+    int num;//local variable
     
-    printf("Enter a number: ");
-    scanf("%d",&num);
-    printf("The number you entered is: ");
-    printf("%d\n",num);
+    printf("Enter a number: ");// printf() dislpays the formatted output
+    scanf("%d",&num);// scanf() reads the formatted input and stores them
+    printf("The number you entered is: ");// printf() dislpays the formatted output
+    printf("%d\n",num);// printf() dislpays the formatted output
     return 0;
 }
